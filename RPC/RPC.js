f2ea01a4e07c5c8491e51fc0971d5fb11ebec904
growthman7 <163215679+growthman7@users.noreply.github.com>
2024-04-02 10:09:10 +0000

    Add files via upload

---
+++ RPC/RPC.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+var bouton= document.getElementById("commencer");
+
+bouton.addEventListener("click", ()=>{
+        alert("BONJOUR, notre jeux consiste a battre notre ordianteur !\n INSTRUCTION: Choisir un bouton et laisser le hasard décider si vous êtes vainqueurs :)");
+
+});
+
